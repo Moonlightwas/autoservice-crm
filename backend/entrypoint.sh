75389034b46ce48e20a04fe274b660435de0253b
Moonlightwas <voloch.nikto@bk.ru>
2026-08-15 00:15:25 +0500

    fix(entrypoint): add admin role

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,7 +7,7 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
 User.objects.filter(email='admin@example.com').exists() or User.objects.create_superuser(
-    email='admin@example.com', password='qwerty@123');
+    email='admin@example.com', password='qwerty@123', role='admin');
 User.objects.filter(email='mechanic@example.com').exists() or User.objects.create_user(
     email='mechanic@example.com', password='qwerty@123', role='mechanic');
 User.objects.filter(email='client@example.com').exists() or User.objects.create_user(
